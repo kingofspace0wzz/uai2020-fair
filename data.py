422d5616b52dcd80a541e9336d0a2f2220fbb929
kingofspace0wzz <zizhuang_wang@umail.ucsb.edu>
2020-02-14 18:20:28 -0800

    u

--- data.py
+++ data.py
@@ -126,8 +126,64 @@ def get_german(path, batch_size, test_size=0.5):
     test_loader = DataLoader(test_data, batch_size, shuffle=True)
     return train_loader, test_loader
 
+def load_crime(path, sensitive='racePactBlack'):
+    column_names = ['{}'.format(i) for i in range(127)]
+    column_names[-1] = 'target'
+    column_names[7] = 'racePctBlack'
+    column_names[27] = 'blackPerCap'
+    column_names[66] = 'PctNotSpeakEnglWell'
+
+    input_data = (pd.read_csv(path, names=column_names,
+                              na_values="?", sep=r'\s*,\s*', engine='python')
+                  )
+    # sensitive attributes; we identify 'race' and 'sex' as sensitive attributes
+    sensitive_attribs = [sensitive]
+    Z = (input_data.loc[:, sensitive_attribs]
+         .assign(sex=lambda df: (df['sensitive'].astype(int) >= 0.2).astype(int)))
+
+    # targets; 1 when someone makes over 50k , otherwise 0
+    y = (input_data['target'].astype(int) >= 0.24).astype(int)
+    
+    # features; note that the 'target' columns are dropped
+    X = (input_data
+        .drop(columns=['target', sensitive])
+        .fillna('Unknown')
+        .pipe(pd.get_dummies, drop_first=True)
+        )
+        # X.insert(0, "sex", Z['sex'])
+        
+
+    print("features X: {} samples, {} attributes".format(X.shape[0], X.shape[1]))
+    print("targets y: {} samples".format(y.shape))
+    print("sensitives Z: {} samples, {} attributes".format(Z.shape[0], Z.shape[1]))
+    return X, y, Z
+
+def get_crime(path, batch_size, test_size=0.5):
+    X, Y, Z = load_crime(path)
+    # split into train/test set
+    (X_train, X_test, y_train, y_test,
+    Z_train, Z_test) = train_test_split(X, Y, Z, test_size=test_size,
+                                        stratify=Y, random_state=7)
+
+    # standardize the data
+    scaler = StandardScaler().fit(X_train)
+    scale_df = lambda df, scaler: pd.DataFrame(scaler.transform(df), 
+                                            columns=df.columns, index=df.index)
+    X_train = X_train.pipe(scale_df, scaler) 
+    X_test = X_test.pipe(scale_df, scaler)
+
+    train_data = CSVDataset(X_train, y_train, Z_train)
+    test_data = CSVDataset(X_test, y_test, Z_test)
+
+    train_loader = DataLoader(train_data, batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size, shuffle=True)
+    return train_loader, test_loader
+
+    
+
+
 if __name__ == "__main__":
-    train_loader, test_loader = get_adult('adult.data', 5)
+    # train_loader, test_loader = get_adult('adult.data', 5)
     total = 0
     l = 0
     # for i, (x, y, z) in enumerate(train_loader):
@@ -137,8 +193,9 @@ if __name__ == "__main__":
     # print(x[:5])
     # print(l / total)
 
-    sampler = torch.utils.data.RandomSampler(train_loader)
+    # sampler = torch.utils.data.RandomSampler(train_loader)
     
+    train_loader, test_loader = get_crime('communities.data', 5)
 
     # train_loader, test_loader = get_german('german.data', 100)
     # total = 0
